c2cf15f654e37209dda7779dea464c80eb5c462f
clemuscle <107462603+clemuscle@users.noreply.github.com>
2024-01-18 14:46:37 +0100

    Ajout instance + création + ajout message + affichage (pour test)

--- serveur/echo_server_tcp.py
+++ serveur/echo_server_tcp.py
@@ -1,12 +1,22 @@
 import socket
-import os
 from _thread import *
+import pymongo
+from datetime import datetime
 
 ServerSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
 ThreadCount = 0
 clients = {}  # Dictionnaire pour stocker les connexions des clients
+
+DATABASE_URL = "mongodb://localhost:27017/"
+DATABASE_NAME = "database_pywhats"
+
+serverDB = pymongo.MongoClient(DATABASE_URL)
+database = serverDB[DATABASE_NAME]
+
+messages_collection = database['messages']
+
 
 try:
     ServerSocket.bind((host, port))
@@ -25,9 +35,33 @@ def threaded_client(connection, client_address):
         if not data:
             break
         
+        # Enregistrement du message avec le timestamp dans la collection MongoDB
+        timestamp = datetime.utcnow()  # Obtient le timestamp actuel en format UTC
+
         # Assume the data received is in the format "recipient: message"
         recipient, message = data.decode('utf-8').split(':', 1)
         
+        # Enregistrement du message dans la collection MongoDB
+        
+        item_1 = [timestamp, client_address, recipient, message]
+
+        document = {
+            'timestamp': item_1[0],
+            'sender': item_1[1],
+            'recipient': item_1[2],
+            'message': item_1[3]
+        }
+
+        messages_collection.insert_one(document)
+
+        test_message = messages_collection.find()
+
+        for test_message in message:
+            print(f"Timestamp: {message['timestamp']}")
+            print(f"Sender: {message['sender']}")
+            print(f"Recipient: {message['recipient']}")
+            print(f"Message: {message['message']}")
+
         # Check if the recipient is a valid connected client
         if recipient in clients:
             recipient_conn = clients[recipient]
