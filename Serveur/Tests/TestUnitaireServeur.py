52251385576be2f6237c6aab3803dc2fb0954201
jBtry <119407693+jBtry@users.noreply.github.com>
2024-02-05 22:15:25 +0100

    Ajustement avant rendu final

--- Serveur/Tests/TestUnitaireServeur.py
+++ Serveur/Tests/TestUnitaireServeur.py
@@ -6,7 +6,6 @@ from Serveur.OutilsServeur import *
 
 mdp_original1 = "motdepasse123"
 mdp_original2 = "motdepasse123."
-compteurTestOk = 0
 
 # Test de la méthode hachage
 print("-----Test de la méthode hachage-----")
@@ -16,9 +15,18 @@ mdp_hache2 = hashmdp(mdp_original2)
 print(f"mdp_original2: {mdp_original2} => mdp_hache2: {mdp_hache2}")
 
 # Test de la méthode de vérification
-print("-----Test de la méthode de vérification du mot de passe -----")
+print("\n-----Test de la méthode de vérification du mot de passe -----")
+
+print(f"Scénario 1 : Test de la méthode vérification avec le mot de passe 1 => {mdp_original1} et le hash associé => {mdp_hache1}")
+print(f"La méthode doit retourner True")
 verification_correcte = verifmdp(mdp_hache1, mdp_original1) # Retourne True
+print(f"Le retour de la méthode est : {verification_correcte}\n")
+
+print(f"Scénario 2 : Test de la méthode de vérification avec le mot de passe 2 => {mdp_original2} et le hash associé au mot de passe 1 => {mdp_hache1}")
+print(f"La méthode doit retourner False")
 verification_incorrecte = verifmdp(mdp_hache1, mdp_original2) # Retourne False
+print(f"Le retour de la méthode est : {verification_incorrecte}\n")
+
 if verification_correcte and not verification_incorrecte :
     print("Le test de la méthode de vérification du mot de passe a réussi")
 else :
